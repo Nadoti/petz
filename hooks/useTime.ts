96e3e3a68ade4faaf5596efd413926d0d84d9b92
Douglas Nadoti <dodo_nadoti@hotmail.com>
2024-01-09 23:49:53 -0300

    Update useTime.ts
    correcao erro horario

--- hooks/useTime.ts
+++ hooks/useTime.ts
@@ -4,7 +4,7 @@ import { errorNotification, notification } from "../utils/notification"
 
 
 const fetcher = (date: string): AxiosPromise<|string[] | undefined> => {
-  return axios.post("http://localhost:3000/api/scheduling/time", {
+  return axios.post("https://petz-seven.vercel.app/api/scheduling/time", {
     date: date
   })
 }
